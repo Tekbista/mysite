f1e3f963bb4bf9ae6f142e3578a80fdf6bd19e6b
Tek Bista <tekbista@Teks-MBP.lan>
2022-12-14 13:05:00 -0500

    added navigation bar and typing animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BodyComponent } from './components/body/body.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -23,6 +23,8 @@ export class FooterComponent implements OnInit {
       window.open('https://www.linkedin.com/in/tek-bista-3496171a5/', '_blank');
     } else if (social === 'facebook') {
       window.open('https://www.facebook.com/profile.php?id=100009128484875', '_blank');
-    } 
+    } else if(social === 'youtube'){
+      window.open('https://www.youtube.com/channel/UCne2j1XHtBDTWmqgTcH2l5Q', "_blank");
+    }
   }
 }
